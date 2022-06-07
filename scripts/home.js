632741929834929e10c175ae611abb93859ea86d
Mateo Bulla T <mateobulla2000@gmail.com>
2022-06-06 20:45:58 -0500

    about us section done

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,7 @@
 //TOGGLE MENU RESPOSIVE
 let menuToggle = document.querySelector(".toggle");
+let navbar = document.querySelector(".navbar");
 menuToggle.onclick = ()=>{
     menuToggle.classList.toggle("active");
-}
+    navbar.classList.toggle("navbar--active");
+};
